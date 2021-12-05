aa682ed9a758b2f020f169bad3bedded3197681b
AhmedHafez94 <gm.ahmed.hafez@gmail.com>
2021-12-05 02:18:16 +0200

    Add recipe data model

---
+++ RecipeSearch/Model/RecipeData.swift
@@ -0,0 +1,33 @@
+//
+//  Recipe.swift
+//  RecipeSearch
+//
+//  Created by Ahmed Hafez on 12/5/21.
+//
+
+import Foundation
+
+
+struct RecipeData: Codable {
+    var q: String
+    var from: Int
+    var to: Int
+    var more: Bool
+    var count: Int
+    var hits: [Hit]
+
+}
+
+struct Hit: Codable {
+    var recipe: Recipe
+}
+
+struct Recipe: Codable {
+    var uri: String
+    var label: String
+    var image: String
+    var source: String
+    var url: String
+    
+    
+}
